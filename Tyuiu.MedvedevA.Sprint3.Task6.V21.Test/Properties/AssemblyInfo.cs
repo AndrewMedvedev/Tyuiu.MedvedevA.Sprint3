673869a4d9e8db65bde9c4fefc5ad923a2f516e4
Андрей Медведев <Tyuiu.MedvedevA@outlook.com>
2023-10-29 18:05:51 +0500

    Task6

---
+++ Tyuiu.MedvedevA.Sprint3.Task6.V21.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MedvedevA.Sprint3.Task6.V21.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MedvedevA.Sprint3.Task6.V21.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("032ed373-a38f-45da-b09f-c0a7967d6023")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.MedvedevA.Sprint3.Task6.V21/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MedvedevA.Sprint3.Task6.V21.Lib;
+
+namespace Tyuiu.MedvedevA.Sprint3.Task6.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService dataService = new DataService();
+
+           
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 19, stopValue = 30;
+
+            Console.WriteLine($"Начало отрезка: {startValue}");
+            Console.WriteLine($"Конец отрезка: {stopValue}");
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write($"Результат = {dataService.GetSumTheDivisors(startValue, stopValue)}");
+
+            Console.ReadKey();
+        }
+    }
+}
