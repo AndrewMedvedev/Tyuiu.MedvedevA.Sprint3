f8cf969ed0139d591baaf5eed6eb2c580ac0f1d1
Андрей Медведев <Tyuiu.MedvedevA@outlook.com>
2023-10-29 19:31:53 +0500

    Task7

---
+++ Tyuiu.MedvedevA.Sprint3.Task7.V16.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.MedvedevA.Sprint3.Task7.V16.Lib
+{
+    public class DataService : ISprint3Task7V16
+
+
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray; 
+            int len = (stopValue - startValue) + 1; 
+            valueArray = new double[len]; 
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round(Math.Sin(x), 2);  
+                valueArray[count] = y; 
+                count++;
+
+            }
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.MedvedevA.Sprint3.Task7.V16.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MedvedevA.Sprint3.Task7.V16.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MedvedevA.Sprint3.Task7.V16.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d0a60bbe-a84e-46d2-a835-bc02d7b7817d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
